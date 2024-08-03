0b7876ac3c3e9cac4498ea8ac294c7512f6176b8
YashleyChua <yashleyjoaquin.chua@benilde.edu.ph>
2024-08-03 18:50:45 +0800

    Implemented Event, Home & Profile Controllers

---
+++ RunBuddies.App/Controllers/EventsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunBuddies.App.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Filter()
+        {
+            return View();
+        }
+    }
+}
---
+++ RunBuddies.App/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunBuddies.App.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
