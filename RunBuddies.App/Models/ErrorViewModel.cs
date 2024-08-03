2c878fbbb09f73b50f618365f50126762cd7d095
YashleyChua <yashleyjoaquin.chua@benilde.edu.ph>
2024-08-03 18:38:22 +0800

    Implemented ASP.NET Core Web App

---
+++ RunBuddies.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RunBuddies.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
